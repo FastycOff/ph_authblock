10fb183d2a2d5e6dfb09ff99070eb2f5ee9ba828
Fastyc <stepan.yurikov.2005@gmail.com>
2021-09-17 15:57:07 +0500

    Update ph_block.js

--- ph_block.js
+++ ph_block.js
@@ -2,10 +2,34 @@ function removeAds()
 {
     let container = document.getElementById("age-verification-container");
     let wraper = document.getElementById("age-verification-wrapper");
+    let welcome = document.getElementById("welcome");
+    let header = document.getElementById("headerMenuContainer");
+    let mainmenu = document.getElementById("headerMainMenuInner");
+    let menu = document.getElementById("js-networkBar");
+    let ad = document.getElementsByClassName("alpha");
+    let Titles = document.getElementsByClassName("sectionTitle");
+    let adallert = document.getElementsByClassName("abAlertInner");
+
 
             // Удаляет рекламу!
             container.setAttribute("style", "display: none !important;");
             wraper.setAttribute("style", "display: none !important;");
+            welcome.setAttribute("style", "display: none !important;");
+            header.setAttribute("style", "display: none !important;");
+            menu.setAttribute("style", "display: none !important;");
+            mainmenu.setAttribute("style", "display: none !important;");  
+            for (let index = 0; index < ad.length; index++) 
+            {
+                ad[index].setAttribute("style", "display: none !important;");
+            }
+            for (let index = 0; index < adallert.length; index++) 
+            {
+                adallert[index].setAttribute("style", "display: none !important;");
+            }
+            for (let index = 0; index < Titles.length; index++) 
+            {
+                Titles[index].setAttribute("style", "display: none !important;");
+            }
 }
 
 
