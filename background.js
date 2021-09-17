41f0d1e3ff3230f57863f2e5ea7b9062c14eec93
Fastyc <stepan.yurikov.2005@gmail.com>
2021-09-17 15:17:44 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,36 @@
-.
+chrome.webNavigation.onCommitted.addListener(function (tab) {
+    // Запрещает запуск скрипта во время загрузки других фреймов
+    if (tab.frameId == 0) {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+
+            // Получает URL страницы
+            let url = tabs[0].url;
+            // Удаляет из URL необязательные определения протоколов и поддомен www
+            let parsedUrl = url.replace("https://", "")
+                .replace("http://", "")
+                .replace("www.", "")
+
+            let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
+                .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
+
+            try {
+                if (domain.length < 1 || domain === null || domain === undefined) {
+                    return;
+                } else if (domain == "rt.pornhub.com") {
+                    runBlockScript();
+                    return;
+                }
+            } catch (err) {
+                throw err;
+            }
+
+        });
+    }
+});
+function runBlockScript() {
+    // Встраивает в страницу скрипт из файла
+    chrome.tabs.executeScript({
+        file: 'PhBlock.js'
+    });
+    return true;
+}
