df4c77595bf32fe7bbf0756ab0e1e6dafca7640e
Fastyc <stepan.yurikov.2005@gmail.com>
2021-09-17 15:17:57 +0500

    Update ph_block.js

--- ph_block.js
+++ ph_block.js
@@ -1,1 +1,12 @@
-.
+function removeAds() 
+{
+    let container = document.getElementById("age-verification-container");
+    let wraper = document.getElementById("age-verification-wrapper");
+
+            // Удаляет рекламу!
+            container.setAttribute("style", "display: none !important;");
+            wraper.setAttribute("style", "display: none !important;");
+}
+
+
+removeAds();
